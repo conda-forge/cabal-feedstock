93b5d2b3f53cb3b391665fe77b13635e2618b833
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-03 00:35:50 +0100

    Explictily setting CC for gcc7

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
+if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
+   export CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc
+fi
 ghc-pkg recache
 cd cabal-install
 export EXTRA_CONFIGURE_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
