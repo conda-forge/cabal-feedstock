f096c78b059c6035dc1b37245c1c3022fb438f16
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-03 00:47:13 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
-if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
-   export CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc
+if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc" ]; then
+   export CC="$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc"
 fi
 ghc-pkg recache
 cd cabal-install
