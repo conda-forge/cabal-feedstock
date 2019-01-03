426556a31006314ada97189d89e24fc4a3c04077
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-03 19:11:54 +0100

    Stripped path segment from CC

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc" ]; then
-   export CC="$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc"
+   export CC="x86_64-conda_cos6-linux-gnu-gcc"
 fi
 ghc-pkg recache
 cd cabal-install
