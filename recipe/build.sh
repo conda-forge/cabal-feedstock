98f0ce381b1348f8ae4d5037d8c6e7fdb52a67aa
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-07 16:49:43 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
+export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
+echo "ENVIRONMENT"
+echo "CC"
+echo "$CC"
+echo "LD"
+echo "$LD"
+echo "Content PREFIX bin"
+ls -lrt $PREFIX/bin
+echo "Content PREFIX lib"
+ls -lrt $PREFIX/lib
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc" ]; then
    export CC="x86_64-conda_cos6-linux-gnu-gcc"
    echo "Setting x86_64-conda_cos6-linux-gnu-gcc"
@@ -9,4 +18,6 @@ fi
 ghc-pkg recache
 cd cabal-install
 export EXTRA_CONFIGURE_OPTS="-threaded --extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
+echo "Extra configure opts"
+echo "$EXTRA_CONFIGURE_OPTS"
 ./bootstrap.sh
