c17fcf74bef4ea39fd00badfcdd0b1660851bbd9
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-09 21:07:15 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,6 @@
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
-echo "ENVIRONMENT"
-echo "CC"
-echo "$CC"
-echo "LD"
-echo "$LD"
-echo "PATH"
-echo "$PATH"
-export PATH="$PREFIX/bin:$PATH"
 echo "Content PREFIX bin"
 ls -lrt $PREFIX/bin
 echo "Content PREFIX lib"
@@ -20,16 +12,18 @@ echo "ALL ENVS"
 env
 
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc" ]; then
+   echo "Setting x86_64-conda_cos6-linux-gnu-gcc"
    x86_64-conda_cos6-linux-gnu-gcc --version
    export CC="gcc"
-   echo "Setting x86_64-conda_cos6-linux-gnu-cc"
    ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $PREFIX/bin/ld
    ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc $PREFIX/bin/gcc
    gcc --version
+   export EXTRA_CONFIGURE_OPTS=" --with-gcc=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc --extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
 else
    echo "Setting LD path"
    export LD_LIBRARY_PATH="/lib64:$LD_LIBRARY_PATH"
    export LIBRARY_PATH="/lib64:$LIBRARY_PATH"
+   export EXTRA_CONFIGURE_OPTS=" --extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
 fi
 echo "which ld"
 which ld
@@ -37,7 +31,6 @@ echo "which gcc"
 which gcc
 ghc-pkg recache
 cd cabal-install
-export EXTRA_CONFIGURE_OPTS=" --ghc-options="-threaded" --extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
 echo "Extra configure opts"
 echo "$EXTRA_CONFIGURE_OPTS"
 ./bootstrap.sh
