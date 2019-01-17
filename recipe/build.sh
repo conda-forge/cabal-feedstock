4437fc5c24a41ca34ec6f587092997bf018f71f1
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-17 11:29:53 +0100

    retrigger

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,7 +28,7 @@ if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc" ]; then
    #x86_64-conda_cos6-linux-gnu-g++ -v test.c 
    export EXTRA_CONFIGURE_OPTS=" --with-gcc=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc --extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
    sed -i -- 's/${GHC} --make ${JOBS} ${PKG_DBS} Setup -o Setup/${GHC} -pgmc ${CC} -pgml ${LD} --make ${JOBS} ${PKG_DBS} Setup -o Setup/g' bootstrap.sh
-else
+else 
    echo "Setting LD path"
    #export LD_LIBRARY_PATH="/lib64:$LD_LIBRARY_PATH"
    #export LIBRARY_PATH="/lib64:$LIBRARY_PATH"
