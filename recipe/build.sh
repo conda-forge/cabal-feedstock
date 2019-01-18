9676c5ed9993bc5bae05e887f8cfcde787359e0d
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-18 12:17:15 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
+export LDFLAGS=""
+export CFFLAGS=""
 echo "Content PREFIX bin"
 ls -lrt $PREFIX/bin
 echo "Content PREFIX lib"
