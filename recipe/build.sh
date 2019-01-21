7f0b16b81fe40602b15b4630a96e6d44affee68f
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-21 23:02:32 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -60,7 +60,7 @@ echo "Extra configure opts"
 echo "$EXTRA_CONFIGURE_OPTS"
 sed -i -- 's/export LD=$LINK/export LINK=x86_64-conda_cos6-linux-gnu-ld/g' bootstrap.sh
 #sed -i -- 's/args="$args ${EXTRA_CONFIGURE_OPTS} ${VERBOSE}"/args="$args ${EXTRA_CONFIGURE_OPTS} ${VERBOSE}"\n echo -e "$args"\n/g' bootstrap.sh
-sed -i -- 's/${GHC} -lgmp -threaded --make ${JOBS} ${PKG_DBS} Setup -o Setup/${GHC} -pgmc ${CC} -pgml ${LD} --make ${JOBS} ${PKG_DBS} Setup -o Setup/g' bootstrap.sh
+sed -i -- 's/${GHC} --make ${JOBS} ${PKG_DBS} Setup -o Setup/${GHC} -lgmp -threaded -pgmc ${CC} -pgml ${LD} --make ${JOBS} ${PKG_DBS} Setup -o Setup/g' bootstrap.sh
 cat bootstrap.sh
 export GHC=`which ghc`
 strings $GHC
