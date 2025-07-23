1b50b96a97482a7de0f38b912066ad11e72d10d1
mementorc <claude.rc@gmail.com>
2025-07-23 08:35:10 -0500

    (fix) -Lxxx

--- recipe/build.sh
+++ recipe/build.sh
@@ -42,7 +42,7 @@ main() {
     export CABAL_CONFIG_FLAGS="--enable-static --disable-shared --ghc-options=-static"
   elif [[ "${target_platform}" == osx-* ]]; then
     settings_file=$(find "${BUILD_PREFIX}"/ghc-bootstrap/lib -name settings)
-    perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -Wl,-rpath,\$topdir/../../../../lib"#g' "${settings_file}"
+    perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -L\$topdir/../../../../lib"#g' "${settings_file}"
     export CABAL_CONFIG_FLAGS="-v1 --enable-static --disable-shared --ghc-options=-optl-Wl,-dead_strip"
   else
     export CABAL_CONFIG_FLAGS=""
