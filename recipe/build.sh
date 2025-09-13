79989e7d85f33a3fcd6fe17de5500e839065ae92
mementorc <claude.rc@gmail.com>
2025-09-13 09:25:22 -0500

    (fix) libgcc-ng

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,42 +33,56 @@ install_cabal() {
 
 # Main build process
 main() {
-  
-  # Don't copy libc - causes segfaults due to version mixing
-  # Configure GHC for Windows compatibility
-  if [[ "${target_platform}" == win-* ]]; then
+  export CABAL=$(find "${SRC_DIR}"/cabal-bootstrap -name "cabal*" -type f | head -1)
+  chmod +x "${CABAL}"
+
+  if [[ "${target_platform}" == "win-"* ]]; then
     export CC=${GCC}
     export CABAL_CONFIG_FLAGS="--enable-static --disable-shared --ghc-options=-static"
-  elif [[ "${target_platform}" == osx-* ]]; then
+    
+  elif [[ "${target_platform}" == "osx-"* ]]; then
     export CABAL_CONFIG_FLAGS="--enable-static --disable-shared --ghc-options=-optl-Wl,-dead_strip"
-  else
+    
+  elif [[ "${target_platform}" == "linux-64" ]]; then
     # Correct the libc.so script to avoid trying to load /lib64/libc.so.6
     sysroot_libc_script="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64/libc.so"
     sed -i "s|/lib64/libc.so.6|libc.so.6|g" "$sysroot_libc_script"
     sed -i "s|/usr/lib64/libc_nonshared.a|libc_nonshared.a|g" "$sysroot_libc_script"
     sed -i "s|/lib64/ld-linux-x86-64.so.2|ld-2.17.so|g" "$sysroot_libc_script"
+ 
+    # Ensure sysrrot use for cabal
+    patchelf --remove-rpath "${CABAL}"
+    patchelf --force-rpath --set-rpath "${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64:${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/ghc-bootstrap/lib/ghc-9.6.7/lib/x86_64-linux-ghc-9.6.7:${BUILD_PREFIX}/x86_64-conda-linux-gnu/lib:${BUILD_PREFIX}/lib" "${CABAL}"
+ 
+    # Set interpreter for compilation by bootstrap
+    patchelf --set-interpreter "${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-2.17.so" "${CABAL}"
     
     export CABAL_CONFIG_FLAGS=""
     export C_INCLUDE_PATH="${BUILD_PREFIX}/include:${C_INCLUDE_PATH:-}"
+    
+  else
+    echo "Conda system ${target_platform} not supported"
+    exit 1
   fi
-
-  export CABAL=$(find "${SRC_DIR}"/cabal-bootstrap -name "cabal*" -type f | head -1)
-  
-  # Ensure sysrrot use for cabal
-  patchelf --remove-rpath "${CABAL}"
-  patchelf --force-rpath --set-rpath "${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64:${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/ghc-bootstrap/lib/ghc-9.6.7/lib/x86_64-linux-ghc-9.6.7:${BUILD_PREFIX}/x86_64-conda-linux-gnu/lib:${BUILD_PREFIX}/lib" "${CABAL}"
-  
-  # Set interpreter for compilation by bootstrap
-  patchelf --set-interpreter "${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-2.17.so" "${CABAL}"
-  chmod +x "${CABAL}"
 
   clean_cabal || true
 
   # Append release project if it exists
   if [[ -f cabal.release.project ]]; then
-      cat cabal.release.project > cabal.release.constraints.project
+    cat cabal.release.project > cabal.release.constraints.project
+  elif [[ -f cabal.project.release ]]
+    cat cabal.project.release > cabal.release.constraints.project
+  elif [[ -f cabal.project ]]; then
+    cat cabal.project > cabal.release.constraints.project
+  else
+    echo "No cabal project file found"
+    exit 1
   fi
 
+  # Remove static linking zlib
+  sed -i 's|package zlib$||' cabal.release.constraints.project
+  sed -i 's|  flags: -pkg-config +bundled-c-zlib||' cabal.release.constraints.project
+  
   # Create project configuration
   cat >> cabal.release.constraints.project << EOF
 allow-newer:
@@ -85,8 +99,10 @@ EOF
     exit 1
   fi
 
-  # Reset interpreter to default (it should work with any libc >= 2.17)
-  patchelf --set-interpreter "/lib64/ld-linux-x86-64.so.2" "${PREFIX}"/bin/cabal
+  if [[ "${target_platform}" == "linux-64" ]]; then
+    # Reset interpreter to default (it should work with any libc >= 2.17)
+    patchelf --set-interpreter "/lib64/ld-linux-x86-64.so.2" "${PREFIX}"/bin/cabal
+  fi
 }
 
 # Run main function
