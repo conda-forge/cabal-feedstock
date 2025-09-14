c5dd98b98d7a7b7dabf54d0b35a8a77382e4e5ef
mementorc <claude.rc@gmail.com>
2025-09-14 10:53:27 -0500

    (fix) arch

--- recipe/build.sh
+++ recipe/build.sh
@@ -44,6 +44,8 @@ main() {
     
   elif [[ "${target_platform}" == "osx-"* ]]; then
     export CABAL_CONFIG_FLAGS="--ghc-options=-optl-Wl,-dead_strip"
+    sed -i 's#[^ ]*libiconv.2.tbd [^ ]*private"##g' "${BUILD_PREFIX}"/ghc-bootstrap/lib/ghc-9.6.7/lib/settings
+
     
   elif [[ "${target_platform}" == "linux-64" ]]; then
     # Correct the libc.so script to avoid trying to load /lib64/libc.so.6
